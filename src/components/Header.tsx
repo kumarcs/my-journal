afad4f6d8890f458498ea6b7be3d510e65f114b5
Nitish Kumar <nitishkumar@192.168.1.10>
2025-05-11 15:41:38 +0530

    corrected code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,15 @@ import info from '../personalData/info.json';
 import '../index.css';
 
 const Header = () => (
-  <header className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-8 shadow-lg">
+  <header className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-2 shadow-lg">
     <div className="container mx-auto flex justify-between items-center">
-      <h1 className="text-4xl font-extrabold">{info.name}</h1>
+      <h1 className="text-4xl font-extrabold m-4">{info.name}</h1>
       <a
         href={info.instagram}
-        className="bg-white text-blue-500 font-semibold py-2 px-4 rounded-full shadow-md hover:bg-gray-100 transition duration-300"
+        className="
+        bg-white text-blue-500 font-semibold py-2 px-4 rounded-full 
+        shadow-md hover:bg-gray-100 transition duration-300
+        m-4"
         target="_blank"
         rel="noopener noreferrer"
       >
